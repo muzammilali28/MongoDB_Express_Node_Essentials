631b41053de696ee60862a58063472d774d88661
Muzammil Ali <muzammilali28@gmail.com>
2023-05-28 16:48:58 +0500

    Increased the Expiration Time JWT Token

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -85,7 +85,7 @@ const loginUser = asyncHandler(async (req, res) => {
             }
         },
             process.env.ACCESS_TOKEN_SECRET_KEY,
-            { expiresIn: "5m" }
+            { expiresIn: "15m" }
         );
 
         res.status(200).json({ token: accessToken })
